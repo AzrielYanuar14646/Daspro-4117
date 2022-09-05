d477815dd42182916903f0a768e74fa0f65b041d
AzrielYanuar14646 <112886826+AzrielYanuar14646@users.noreply.github.com>
2022-09-05 23:01:21 +0700

    Add files via upload

---
+++ Tugas 1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Muhamad Azriel Yanuar" << endl;
+    cout << "A11.2022.14646" << endl;
+    cout << "SMAN 1 KAJEN" << endl;
+    return 0;
+}
