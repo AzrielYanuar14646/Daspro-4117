11848f60ee75789dde69353ec502ea0c27a1ff26
AzrielYanuar14646 <112886826+AzrielYanuar14646@users.noreply.github.com>
2022-09-12 22:17:01 +0700

    Add files via upload

---
+++ pertemuan 3/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello!" << endl;
+
+    string nama;
+    cout << "Nama Anda : ";
+    cin >> nama;
+
+    cout << "Hi " << nama;
+    cout << " selamat datang!\n" << endl;
+
+    cout << "Muhamad Azriel Yanuar \nA11.2022.14646" << endl;
+
+    int u = 18;
+    float t = 164.5;
+    char b = 'L';
+    int s = 42;
+
+
+
+    cout << "\nUmur Saya : " << u;
+    cout << endl;
+    cout << "Tinggi Saya : " << t;
+    cout << endl;
+    cout << "Ukuran Baju : " << b;
+    cout << endl;
+    cout << "Ukuran Sepatu : " << s;
+    cout << endl;
+
+    return 0;
+}
